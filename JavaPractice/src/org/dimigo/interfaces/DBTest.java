20517a2242641f988c859423322e65ac99dc8e08
김민선 <@MINSUN>
2017-05-25 16:11:47 +0900

    0

---
+++ JavaPractice/src/org/dimigo/interfaces/DBTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ * <pre>
+ * org.dimigo.interfaces
+ *    |_ DBTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 25.
+ * </pre>
+ * 
+ *
+ * @author          :김민선
+ * @version         : 1.0
+ */
+public class DBTest {
+	   public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+	      System.out.println("< 변경 전 >");
+	      crub(IDBManager.getDBObject(IDBManager.SYBASE_DATABASE));
+	      System.out.println("\n< 변경 후 >");
+	      crub(IDBManager.getDBObject(IDBManager.ORACLE_DATABASE));
+
+	   }
+	   private static void crub(IDBManager db)
+	   {
+	      db.insert();
+	      db.search();
+	      db.update();
+	      db.delete();
+	   }
+	}
